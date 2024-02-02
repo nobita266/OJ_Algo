f6a28af1c5eecdb2fa13e68ad3aa3342035de442
Nobita <yash.saini01230@gmail.com>
2024-02-02 17:35:29 +0530

    delete the .git from the frontend

--- backend/index.js
+++ backend/index.js
@@ -4,34 +4,50 @@ const { DBConnection } = require("./database/db");
 const User = require("./model/user");
 const { default: mongoose } = require("mongoose");
 var bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 const app = express();
 const PORT = process.env.PORT || 8080;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 DBConnection();
 app.post("/signup", async (req, res) => {
-  //get all the data from frontend
-  const { firstname, lastname, email, password } = req.body;
+  try {
+    //get all the data from frontend
+    const { firstname, lastname, email, password } = req.body;
 
-  //check all the data should exist
-  if (!(firstname && lastname && email && password)) {
-    return res.status(400).send("please enter all required fields!");
+    //check all the data should exist
+    // if (!firstname && !lastname && !email && !password) {
+    //   return res.status(400).send("please enter all required fields!");
+    // }
+    //check if the user already exist or not
+    const isUserExist = await User.findOne(email);
+    if (isUserExist) {
+      return res.status(200).send("user with this email already exist");
+    }
+
+    //encrypt the user password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // save the user data into db
+    const userData = await User.create({
+      firstname,
+      lastname,
+      email,
+      password: hashPassword,
+    });
+
+    //generate a token  fr user and sent it(JWT token)
+    const token = jwt.sign({ id: User._id, email }, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    userData.token = token;
+    userData.password = undefined;
+    res.status(200).json({
+      message: "You have successfully registered!",
+    });
+  } catch (error) {
+    console.log("Error :" + error.message);
   }
-  //check if the user already exist or not
-  const isUserExist = await User.findOne(email);
-  if (isUserExist) {
-    return res.status(200).send("user with this email already exist");
-  }
-
-  //encrypt the user password
-  const hashPassword = await bcrypt.hash(password, 10);
-  const userData = await User.create({
-    firstname,
-    lastname,
-    email,
-    password: hashPassword,
-  });
-
-  // save the user data into db
-  //generate a token  fr user and sent it(JWT token)
 });
 app.post("/login", (req, res) => {
   res.send("login page");
